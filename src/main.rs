c587fb6f647c57b24a1a43b20e20fed0554c20cd
jmcook413 <jeremycook290@gmail.com>
2019-01-22 20:57:42 -0600

    Adds a function that iterates fibonacci numbers.
    This commit adds a function called `find_the_number`, and adds a bit
    more code to the main function. The other additions, to the main
    function, will help deal with the incoming index to find the desired
    fibonacci number.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let fibonacci_index: u128 = args[1].trim().parse()
+        .expect("Please enter a positive integer greater than 0!");
+
+    find_the_number(fibonacci_index);
+}
+
+fn find_the_number(index: u128) {
+    let mut f_num: u128 = 0;
+    let mut f_in: u128 = 1;
+    let mut f_temp: u128;
+
+    for _i in 1..index {
+        f_temp = f_num + f_in;
+
+        f_num = f_in;
+        f_in = f_temp;
+    }
+
+    println!("The fibonacci number with an index of {} is {}.", index, f_num);
 }
